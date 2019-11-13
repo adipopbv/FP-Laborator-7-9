30cb40511aec6c898e3d25fee6260706dbf25c36
PridesFall08 <adi.pop.bv@gmail.com>
2019-11-13 17:38:15 +0200

    Added ui.gui package with console module

---
+++ EventOrganiser/ui/gui/console.py
@@ -0,0 +1,13 @@
+class Console:
+
+    def __init__(self):
+        pass
+
+    def Print(self, message):
+        """
+        outputs message in teh console
+        
+        Args:
+            message (str): text to be outputted
+        """
+        print(message)
