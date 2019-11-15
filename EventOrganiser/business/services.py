90c9357b1ef2923e352d5c6c3c049485abd81a29
PridesFall08 <adi.pop.bv@gmail.com>
2019-11-15 15:48:59 +0200

    Added service-side adding person to repo functionality

--- EventOrganiser/business/services.py
+++ EventOrganiser/business/services.py
@@ -1,10 +1,11 @@
-
+from domain.entities import Id, Name, CityName, StreetName, Number, Adress, Person
 
 class Service:
     
-    def __init__(self, persons, events):
+    def __init__(self, persons, events, validator):
         self._persons = persons
         self._events = events
+        self._validator = validator
 
     def get_persons(self):
         return self._persons
@@ -12,7 +13,27 @@ class Service:
     def get_events(self):
         return self._events
 
+    def get_validator(self):
+        return self._validator
+
     #-----------------------------------
 
+    def create_adress(self, city, street, number):
+        __city = CityName(city)
+        __street = StreetName(street)
+        __number = Number(number)
+        return Adress(__city, __street, __number)
+
+    def create_person(self, id, name, adress):
+        __id = Id(id)
+        __name = Name(name)
+        return Person(__id, __name, adress)
+
     def add_person_to_repo(self, repo, id, name, city, street, number):
-        pass
+        try:
+            person = self.create_person(id, name, self.create_adress(city, street, number))
+            self.get_validator().validate_person(person)
+            self.get_persons().add(person)
+            return repo
+        except Exception as ex:
+            raise Exception(ex)
