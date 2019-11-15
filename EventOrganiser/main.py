f7bd708a8398779599cdc81d79e313ab2347c92d
PridesFall08 <adi.pop.bv@gmail.com>
2019-11-15 15:48:17 +0200

    Added validation

--- EventOrganiser/main.py
+++ EventOrganiser/main.py
@@ -1,10 +1,12 @@
 from ui.clients import Client
 from business.services import Service
 from framework.repos import Repo
+from framework.validators import Validator
 
 persons = Repo()
 events = Repo()
-service = Service(persons, events)
+validator = Validator()
+service = Service(persons, events, validator)
 client = Client(service)
 
 client.run()
