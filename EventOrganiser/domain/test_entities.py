7aab6c8852cf6ffeda9a7f65a83b125b8b737c86
PridesFall08 <adi.pop.bv@gmail.com>
2019-12-04 13:43:29 +0200

    Added test case for person

--- EventOrganiser/domain/test_entities.py
+++ EventOrganiser/domain/test_entities.py
@@ -1,5 +1,7 @@
 import unittest
-from EventOrganiser.domain.entities import Command, Entity
+from EventOrganiser.domain.entities import Command, Entity, Person
+from EventOrganiser.domain.exceptions import NotListException
+from EventOrganiser.domain.fields import Address
 
 
 class TestCaseCommand(unittest.TestCase):
@@ -24,8 +26,40 @@ class TestCaseEntity(unittest.TestCase):
         self.assertEqual(self.entity, e)
 
     def test_to_json(self):
-        d = { "id": "id" }
+        d = {"id": "id"}
         self.assertEqual(self.entity.to_json(), d)
+
+
+class TestCasePerson(unittest.TestCase):
+    def setUp(self):
+        self.address = Address("city", "street", "number")
+        self.person = Person("id", "name", self.address)
+
+    def test_equal(self):
+        p = Person("id", "name", Address("city", "street", "number"))
+        self.assertTrue(self.person == p)
+
+    def test_to_json(self):
+        d = {
+            "id": "id",
+            "name": "name",
+            "address": self.address.to_json()
+        }
+        self.assertEqual(self.person.to_json(), d)
+
+    def test_has_field_with_value(self):
+        self.assertTrue(self.person.has_field_with_value("id", "id"))
+        self.assertFalse(self.person.has_field_with_value("id", "no"))
+        self.assertFalse(self.person.has_field_with_value("no", "id"))
+        self.assertTrue(self.person.has_field_with_value("city", "city"))
+        self.assertFalse(self.person.has_field_with_value("city", "no"))
+        self.assertFalse(self.person.has_field_with_value("address", "city"))
+
+    def test_not_in_list(self):
+        self.assertTrue(self.person.not_in_list([]))
+        self.assertTrue(self.person.not_in_list([1, "stuff"]))
+        self.assertFalse(self.person.not_in_list([self.person, 2]))
+        self.assertRaises(NotListException, self.person.not_in_list, 12)
 
 
 if __name__ == '__main__':
